73356b1994e036f07e2cb45c15df050367eaaf4e
andre custodio <andre custodio>
2017-04-01 18:42:50 -0300

    Fixes

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,7 +5,7 @@ module.exports = function (server) {
     server.set('port', process.env.PORT || 8080);
 
     const router = express.Router()
-    server.use('/api', router)
+    server.use('/api/finances', router)
 
     //Rotas de Servicos
     const Servicos = require('../api/services/servicosService')
